673392cd907fe3db764da3f195d95a66f809f780
Fleuv <fleuv@live.com>
2020-09-25 12:40:09 +0200

    Fixed find replace

--- .github/scripts/conf-map-file-name.sh
+++ .github/scripts/conf-map-file-name.sh
@@ -2,8 +2,6 @@
 
 version="$MAP_VERSION"
 base_name="$MAP_BASE_NAME"
-file_name="$MAP_FILE_NAME"
-
-file_name=sed -e "s/\[MAP_BASE_NAME]/$base_name/" -e "s/\[MAP_VERSION]/$version/"
+file_name=$(echo "$MAP_FILE_NAME" | sed -e "s/\[MAP_BASE_NAME]/$base_name/" -e "s/\[MAP_VERSION]/$version/")
 
 echo ::set-env name=MAP_FILE_NAME::"$file_name"
